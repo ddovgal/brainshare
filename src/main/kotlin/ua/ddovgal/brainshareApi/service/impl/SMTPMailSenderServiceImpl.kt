5d759748cb43be103a28520370b716a4101281fc
Dmitriy Dovgal <errorer.404@gmail.com>
2017-05-31 14:24:49 +0300

    Update SMTPMailSenderServiceImpl.kt

--- src/main/kotlin/ua/ddovgal/brainshareApi/service/impl/SMTPMailSenderServiceImpl.kt
+++ src/main/kotlin/ua/ddovgal/brainshareApi/service/impl/SMTPMailSenderServiceImpl.kt
@@ -1,7 +1,6 @@
 package ua.ddovgal.brainshareApi.service.impl
 
 import org.springframework.mail.javamail.JavaMailSender
-import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Service
 import ua.ddovgal.brainshareApi.configuration.MailProperties
 import ua.ddovgal.brainshareApi.service.MailSenderService
@@ -17,7 +16,6 @@ class SMTPMailSenderServiceImpl(
 
     private val FROM_ADDRESS_NAME = "Brainshare Support"
 
-    @Async
     override fun sendSimpleMessage(receiverAddress: String, receiverName: String, subject: String, text: String) {
         val message = sender.createMimeMessage()
         message.setFrom(InternetAddress(mailProperties.fromAddress, FROM_ADDRESS_NAME))
